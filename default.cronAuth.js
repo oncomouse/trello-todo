f9f84e405f863b2ce06ee528fe861f71e51f0995
oncomouse <oncomouse@gmail.com>
2017-08-14 10:36:33 -0500

    How to use

--- default.cronAuth.js
+++ default.cronAuth.js
@@ -1,3 +1,5 @@
+// After you have entered your information, rename this file to "cronAuth.js"
+
 const API_TOKEN = ''; // https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&name=Server%20Token&key=412b4a7c515817fb346d925e6f2ad4c2
 const API_KEY = ''; // https://trello.com/app-key
 
