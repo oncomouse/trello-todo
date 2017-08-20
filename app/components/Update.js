b239ac1fd8b655459285efd5646c94dd442b70ab
oncomouse <oncomouse@gmail.com>
2017-08-20 10:49:41 -0500

    Fixed problems with update

--- app/components/Update.js
+++ app/components/Update.js
@@ -4,6 +4,6 @@ import styles from 'stylesheets/components/Update.scss'
 export default (props) => (
 	<div className={`${props.count !== null ? styles.shown : styles.hidden} bg-primary ${(props.submitted !== false) ? styles.hidden : ''}`}>
 		<p><strong>Updating...</strong></p>
-		<p>Posted {props.count} {props.count === 1 ? 'story' : 'stories'}.</p>
+		<p>Posted {props.count} card{props.count === 1 ? '' : 's'}.</p>
 	</div>
 )
