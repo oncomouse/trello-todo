4908c551efd5e5fbfacd1e7cd5dd2bb13182ac4d
oncomouse <oncomouse@gmail.com>
2017-08-20 10:23:08 -0500

    Code to make parser testable

--- app/models/Card.js
+++ app/models/Card.js
@@ -1,5 +1,5 @@
 import {Record} from 'immutable'
-import {HOME_LABEL_NAME} from 'constants/Config'
+import {HOME_LABEL_NAME} from '../constants/Config'
 import chrono from 'chrono-node'
 
 export default class Card extends Record({name: '', desc: '', idLabels: '', idList: null, label: HOME_LABEL_NAME, due: chrono.parseDate('next week'), times: 1}) {}
